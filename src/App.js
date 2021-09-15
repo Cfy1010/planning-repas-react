2771c1bb4a64b77de5a5ea5c3b9f1bf64bc98990
Cfy1010 <cy_one2000@yahoo.fr>
2021-09-15 19:25:37 +0200

    test api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import MealList from "./components/MealList"
 
 function App() {
+  const [mealData, setMealData] = useState(null);
+  const [calories, setCalories] = useState(2000);
+
+  const handleChange = (e) => { setCalories(e.target.value)};
+  
+  const getMealData = () => {
+    fetch(
+      `https://api.spoonacular.com/mealplanner/generate?apiKey=7f30ad9a028145c4934ac66cb670918b&timeFrame=day&targetCalories=${calories}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setMealData(data);
+        console.log(data);
+      })
+      .catch(() => {
+        console.log('error');
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <section className='controls'>
+        <input
+          type='number '
+          placeholder='Calories'
+          onChange={handleChange}
+        />
+      </section>
+      <button onClick={getMealData}>Mon plan repas quotidien</button>
+      {mealData && <MealList MealList={ mealData}/>}
     </div>
   );
 }
